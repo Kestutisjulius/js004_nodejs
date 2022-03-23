66700d3747d3c23c4c5ef73f0cdb592c43187e4e
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-03-23 08:25:13 +0200

    string to number + round

--- files.js
+++ files.js
@@ -2,11 +2,25 @@ const fs = require('fs/promises');
 
     (async () => {
         const content = await fs.readFile('./data/drinks.txt', 'utf-8');
-        console.log(typeof content);
+        //console.log(typeof content);
+        const drinksList = content.split('\r\n');
 
-        for (const letter of content) {
-            console.log(letter);
+        let i = 1;
+        for (const drinks of drinksList) {
+            
+            const eilute = drinks.split(' ');
+            // const pavadinimas = eilute[0];
+            // const kaina = eilute[1];
+            // const kiekis = eilute[2];
+            const [name, price, count]=eilute;
+            const balance = +price * +count;
+            const precision = 2;
+            const b1 = balance.toFixed(precision);
+            const b2 = Math.round(balance * (10 ** precision)) / (10 ** precision);
+            console.log( `Pasirinkimas:${i++}-${name}, kurio kaina yra ${price}, EUR buvo parduota ${count} ir apyvarta ${b2} EUR` );
         }
+        
+
     })();
 
 
